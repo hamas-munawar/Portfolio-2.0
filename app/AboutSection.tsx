bb2e639fb545306610f2163c8bce949cdeea0a1a
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-08-10 22:14:59 +0500

    style(AboutSection): update text colors for better readability

--- app/AboutSection.tsx
+++ app/AboutSection.tsx
@@ -53,18 +53,18 @@ const AboutSection = () => {
       className="min-h-screen grid place-content-center gap-6 md:gap-12 xl:px-20"
       ref={container}
     >
-      <p className="font-thin text-2xl sm:text-4xl md:text-5xl md:pr-12 text-heading slide-up-and-fade">
+      <p className="font-thin text-2xl sm:text-4xl md:text-5xl md:pr-12 text-white-ghost slide-up-and-fade">
         I believe in a user-centered design approach, where every project I work
         on is tailored to meet the specific needs of its users.
       </p>
-      <p className="border-b-[1] border-gray-600 py-4 text-text slide-up-and-fade">
+      <p className="border-b-[1] border-gray-medium py-4 text-white-whisper slide-up-and-fade">
         This is me
       </p>
       <div className="flex flex-col md:flex-row gap-8 mx-auto">
-        <h2 className="grow-1 text-2xl sm:text-3xl md:text-4xl slide-up-and-fade">
+        <h2 className="grow-1 text-2xl sm:text-3xl md:text-4xl slide-up-and-fade text-white-ghost">
           Hi, I'm Hamas.
         </h2>
-        <div className="w-full md:w-[60%] space-y-4 md:pr-10 lg:pr-24 text-text slide-up-and-fade text-lg md:text-xl">
+        <div className="w-full md:w-[60%] space-y-4 md:pr-10 lg:pr-24 text-white-whisper slide-up-and-fade text-lg md:text-xl">
           <p className="slide-up-and-fade">
             I am a full-stack web developer dedicated to turning ideas into
             creative solutions. I specialize in crafting seamless and intuitive
