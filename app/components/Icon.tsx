3b69de3d2c7c875c178767d4dc3f272e4905ac1b
Hamas Munawar <muhammadhamasmunawar@hotmail.com>
2025-08-10 19:33:43 +0500

    feat(components): add Icon component for reusable svg icons

---
+++ app/components/Icon.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import React from "react";
+import { IconName } from "../types/icons";
+
+interface IconProps {
+  name: IconName;
+  size?: string;
+  svgClassNames?: string;
+  parentClassNames?: string;
+  children?: React.ReactNode;
+}
+
+const Icon: React.FC<IconProps> = ({
+  name,
+  size = "24",
+  svgClassNames = "",
+  parentClassNames = "",
+  children,
+}) => {
+  return (
+    <div className={`flex items-center ${parentClassNames}`}>
+      <Image
+        src={`/assets/Icons/${name}.svg`}
+        alt={`${name} icon`}
+        width={parseInt(size)}
+        height={parseInt(size)}
+        className={svgClassNames}
+      />
+      {children && <span>{children}</span>}
+    </div>
+  );
+};
+
+export default Icon;
